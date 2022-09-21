36e9ab1a19045f3fcf9690349734194dd8822db8
trmrskr <simonalale@gmail.com>
2022-09-21 06:54:32 -0700

    print uppercase of a string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@
 def uppercase(str):
     for xter in str:
         xter_ord = ord(xter)
-        if xter_ord >= 97 and xter_ord <= 122:
-            print("{:c}".format(xter_ord-32), end='')
-        else:
-            print("{:c}".format(xter_ord), end='')
+        lowercase = xter_ord >= 97 and xter_ord <= 122
+        print("{:c}".format(xter_ord-32 if lowercase else xter_ord), end='')
     print("")
