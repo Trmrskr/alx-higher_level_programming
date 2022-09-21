7121740f5a0bacc8b9ea1be7eec185a8c3b6e761
trmrskr <simonalale@gmail.com>
2022-09-21 05:25:52 -0700

    print combination of 2 digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for tenth_digit in range(10):
+    for unit_digit in range(10):
+        if unit_digit > tenth_digit:
+            if tenth_digit == 8:
+                if unit_digit == 9:
+                   print("{:d}{:d}".format(tenth_digit, unit_digit))
+                else:
+                    print("{:d}{:d}".format(tenth_digit, unit_digit), end=", ")
+            else:
+                print("{:d}{:d}".format(tenth_digit, unit_digit), end=", ")
