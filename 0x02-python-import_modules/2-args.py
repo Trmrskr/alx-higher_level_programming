58ec9a7a675f421a17a2a144f48b309bc2f53cdb
Alale Tamara-kuro Simon <31554894+Trmrskr@users.noreply.github.com>
2022-09-23 02:15:22 +0100

    major overhaul 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
 if __name__ == "__main__":
     import sys
-    print_arg(sys.argv)
+    args = sys.argv
+    args = args[1:]
+    i = 1
+    if len(args) == 1:
+        print("1 argument:")
+        print("1: {}".format(args[0]))
+    elif len(args) == 0:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(args)))
+        for each in args:
+            print("{}: {}".format(i, each))
+            i += 1
