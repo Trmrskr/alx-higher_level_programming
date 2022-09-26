2a27030432034d1d84c66f4d06b10331d4aea1d5
trmrskr <simonalale@gmail.com>
2022-09-26 09:09:05 -0700

    Maxim completed

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,8 @@
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    maxim = my_list[0]
+    for xger in my_list:
+        if xger > maxim:
+            maxim = xger
+    return maxim
