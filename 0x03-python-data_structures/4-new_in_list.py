979726bec58dc8b1ec3840fc1c0b66bac553803c
trmrskr <simonalale@gmail.com>
2022-09-26 06:06:58 -0700

    added a new comment

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
+    # my_list.copy() can be used to make copy of list
     new_list = my_list[:]
     if idx < 0:
         return new_list
