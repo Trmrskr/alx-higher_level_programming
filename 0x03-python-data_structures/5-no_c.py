b36baa357e092d5cbe7e032c91ecd175a3415b2a
trmrskr <simonalale@gmail.com>
2022-09-26 07:21:49 -0700

    function that removes all character c and C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    my_str_list = list(my_string)
+    i = 0
+    for xter in my_str_list:
+        xter_ord = ord(xter)
+        if xter_ord == 99 or xter_ord == 67:
+            my_str_list.pop(i)
+        i = i + 1
+    return ''.join(str(x) for x in my_str_list)
