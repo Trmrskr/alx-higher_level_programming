52faf35c3a144be5d375d780212a2b1dc33644cd
trmrskr <simonalale@gmail.com>
2022-09-26 20:16:34 -0700

    done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,6 +5,6 @@ def divisible_by_2(my_list=[]):
     for xgers in my_list:
         if xgers % 2 == 0:
             new_list.append(True)
-    return new_list
+        else:
             new_list.append(False)
-
+    return new_list
