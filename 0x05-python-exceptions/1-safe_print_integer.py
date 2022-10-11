47d77d5c2885aef21ed719e9e24fee96f926cc0f
trmrskr <simonalale@gmail.com>
2022-10-10 22:22:38 -0700

    Exception as error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
-    except Exception:
+    except Exception as error:
         return False
     else:
         return True
