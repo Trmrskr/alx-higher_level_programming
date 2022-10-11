bc604c5f8004fbed5de0fe1c2984fd9ec9626bc7
trmrskr <simonalale@gmail.com>
2022-10-10 22:09:14 -0700

    Mini correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[idx]), end="")
             count += 1
         print()
-    except:
-        print();
+    except IndexError:
+        print()
     finally:
         return count
