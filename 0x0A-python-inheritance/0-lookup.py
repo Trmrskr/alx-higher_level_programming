36ff0e7c344a62ca557b14fdd36fa738684ea32c
trmrskr <simonalale@gmail.com>
2022-10-24 13:24:37 -0700

    Task 0-Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This file contains the look up function"""
+
+
+def lookup(obj):
+    """This function returns the list of attributes and
+    methods of the given object"""
+
+    return dir(obj)
