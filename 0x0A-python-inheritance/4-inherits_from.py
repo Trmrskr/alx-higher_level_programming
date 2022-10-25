afac7b67ec19c1aac5befb548f615aa6ab4480f6
trmrskr <simonalale@gmail.com>
2022-10-24 21:06:38 -0700

    Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module contains inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """The function checks if obj inherits_from a_class
+
+    Argument:
+        obj: the object to be checked
+        a_class: the class to be compared
+
+    Return: True if obj is an instance of class otherwise false
+    """
+    if issubclass(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
