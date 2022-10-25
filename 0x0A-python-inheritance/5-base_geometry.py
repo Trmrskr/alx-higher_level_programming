667f188ee2375623688c9c01b8cd69ad5be6af82
trmrskr <simonalale@gmail.com>
2022-10-24 21:11:52 -0700

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains an empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry just simply pass when instantiated
+
+    Attribute: None
+    """
+
+    pass
