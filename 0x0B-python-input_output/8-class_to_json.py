fe6ef618ea2a9461aceb3a2c0c864771336a086f
trmrskr <simonalale@gmail.com>
2022-10-29 07:46:10 +0000

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This file contains the class_to_json function"""
+
+
+def class_to_json(obj):
+    """function returns the obj dictionary of obj
+    
+    Argument:
+        obj: the object whose dictionary description is to be returned
+
+    Return:
+        the obj.__dict__
+    """
+    return obj.__dict__
