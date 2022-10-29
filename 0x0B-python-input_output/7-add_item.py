57b279887ee11459cbce15664f64907abbc9ed9f
trmrskr <simonalale@gmail.com>
2022-10-29 11:44:58 +0000

    Load, add, save: pycodestyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" This module contains a script which loads and offloads from a json file """
 
 import sys
 import os
@@ -9,6 +10,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 if filename not in os.listdir(os.getcwd()):
+    """
+        if file is not in list of files and directory in current
+        working directory do the following
+    """
     my_list = []
     save_to_json_file(my_list, "add_item.json")
 else:
