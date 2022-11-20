69e268654a34e8c37c19ad6b79e09e731ee08f2c
trmrskr <simonalale@gmail.com>
2022-11-20 21:29:28 +0000

    Minor corrections

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 # test_base.py
-"""This module contains the unittests for all custom methods of the base.py.
-
-Unittest classes:
-    TestBase_instantiation - line 23
-    TestBase_to_json_string - line 110
-    TestBase_save_to_file - line 156
-    TestBase_from_json_string - line 234
-    TestBase_create - line 288
-    TestBase_load_from_file - line 340
-    TestBase_save_to_file_csv - line 406
-    TestBase_load_from_file_csv - line 484
+"""This module contains the unittests for all custom methods of the base.py
+    which includes the following:
+    TestBase_instantiation
+    TestBase_to_json_string
+    TestBase_save_to_file
+    TestBase_from_json_string
+    TestBase_create
+    TestBase_load_from_file
+    TestBase_save_to_file_csv
+    TestBase_load_from_file_csv
 """
 import os
 import unittest
