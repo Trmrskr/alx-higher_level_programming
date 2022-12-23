f5a95d5fce511199be54879675b4784c5f285279
trmrskr <simonalale@gmail.com>
2022-12-23 15:17:19 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-This script takes in an argument and
-displays all values in the states
-where `name` matches the argument
-from the database `hbtn_0e_0_usa`.
+This script takes in four arguments, user, password, database and a searchtext
+It uses python MySQLdb connector to query all values in states which name
+matches the searchtext from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -11,18 +10,23 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    The namespace of the script.
+    connect to database using MySQLdb connector.
+    use cursor to create a working environment to interface with database
+    fetchall rows and print using for loop.
     """
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                           passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
+    cur = conn.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE name LIKE BINARY '{}' \
+                 WHERE name = '{}' \
                  ORDER BY states.id ASC".format(argv[4]))
-    rows = cur.fetchall()
+    query_rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+    for rows in query_rows:
+        print(rows)
+
+    cur.close()
+    conn.close()
