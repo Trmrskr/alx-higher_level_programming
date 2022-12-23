d46a02434d67a9dca42ab6c00e067c2598f81c41
trmrskr <simonalale@gmail.com>
2022-12-23 14:03:44 +0000

    Get all states editted

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+This script uses the python MySQLdb connector to connect to MySQL,
+Executes a command that lists all the states from the database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
@@ -9,15 +9,16 @@ from sys import argv
 
 if __name__ == '__main__':
     """
-    Access to the database and get the states
-    from the database.
+    The namespace of this script, creates the connection,
+    use cursor to create a distinct working environment for connection
+    executes an SQL query and print each each row from the SQL queries.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+    conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                          passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
+    cur = conn.cursor()
     cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    query_rows = cur.fetchall()
 
-    for row in rows:
+    for row in query_rows:
         print(row)
