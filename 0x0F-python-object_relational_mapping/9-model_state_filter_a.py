f37cadc197fb41628c30f93924f74e1437b77b7b
trmrskr <simonalale@gmail.com>
2022-12-31 11:33:51 +0000

    Contains 'a'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-This script lists all State objects
-that contain the letter `a`
-from the database `hbtn_0e_6_usa`.
+This python script that use sqlalchemy to query a database and list
+all State objects containing the letter `a` from `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -12,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database and get a state
-    from the database.
+    Connect to the database using db_uri, create a session and query
+    the database
     """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
