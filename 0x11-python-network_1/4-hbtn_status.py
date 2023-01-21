300e552291f533f0ee36005a197430529e498568
trmrskr <simonalale@gmail.com>
2023-01-21 08:42:34 -0800

    What's my status? #1-1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 if __name__ == '__main__':
     import requests
     res = requests.get('https://alx-intranet.hbtn.io/status')
+    txt = res.text
     print('Body response:')
-    print('\t- type: {}'.format(type(res)))
-    print('\t- content: {}'.format(res.text))
+    print('\t- type: {}'.format(type(txt)))
+    print('\t- content: {}'.format(txt))
