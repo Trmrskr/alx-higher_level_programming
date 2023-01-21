663b5d5da624bd712590d562aa0ee291e1349100
trmrskr <simonalale@gmail.com>
2023-01-21 06:52:06 -0800

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 This script takes in a URL, sends a request to the URL and
-Displays the value of the X-Request-Id variable found in the header of the response.
+Displays the value of the X-Request-Id variable found in the
+header of the response.
 """
 
 if __name__ == '__main__':
