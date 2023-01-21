d5372a44711584ba4b716237efbc19970cecd4e1
trmrskr <simonalale@gmail.com>
2023-01-21 08:45:37 -0800

    Response header value #1-2

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+A script that takes in a URL, sends a request to the URL and displays
+the value of the variable X-Request-Id in the response header
+"""
 
 if __name__ == '__main__':
     import requests
