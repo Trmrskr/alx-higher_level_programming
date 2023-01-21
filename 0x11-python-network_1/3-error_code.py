0734d7433ef76e94aa69de47b276968b4e767815
trmrskr <simonalale@gmail.com>
2023-01-21 08:29:48 -0800

    Error code #0: pycodestyle correction

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-Write a python script that takes in a URL, sends a request to the URL and 
+Write a python script that takes in a URL, sends a request to the URL and
 displays the body of the response (decoded in utf-8)
 """
 
