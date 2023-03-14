46f40dfa46dcab1025028fb3a42787730b7742a8
Trmrskr <simonalale@gmail.com>
2023-03-14 15:12:11 +0000

    Code restructuring correction

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,8 @@
 
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-languages.map(language => console.log(language));
-/* for (const str of arrayString) {
-  console.log(str);
-} */
+/* languages.map(language => console.log(language)); */
+
+for (const language of languages) {
+  console.log(language);
+}
