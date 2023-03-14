a40e740b6ae56e97f144ffdef37b9f40014a2576
Trmrskr <simonalale@gmail.com>
2023-03-14 16:51:53 +0000

    Second biggest: code cleaning

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-if (process.argv.length < 4) {
+const len = process.argv.length;
+
+if (len < 4) {
   console.log('0');
 } else {
-  const size = process.argv.length;
   const numbers = [];
-  for (let i = 2; i < size; i++) {
+  for (let i = 2; i < len; i++) {
     numbers[i - 2] = parseInt(process.argv[i]);
   }
   numbers.sort(function (a, b) { return b - a; });
