6f1cb6ca78739977a88e67147aac8fbc1a2f9d76
trmrskr <simonalale@gmail.com>
2023-03-17 07:36:36 -0700

    filter states with the left function

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE states.name LIKE '^N\w*$' \
+                 WHERE LEFT(states.name, 1)== 'N' \
                  ORDER BY states.id ASC")
     rows = cur.fetchall()
 
