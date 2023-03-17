e97bed1080a54ffec548d4233d1969afad66dc4a
trmrskr <simonalale@gmail.com>
2023-03-17 07:44:36 -0700

    filter states using ORD

--- 0x0F-python-object_relational_mapping/redone/1-filter_states_1.py
+++ 0x0F-python-object_relational_mapping/redone/1-filter_states_1.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states \
-                 WHERE ORD(states.name) = 78 \
+                 WHERE LEFT(states.name, 1)= 'N' \
                  ORDER BY states.id ASC")
     rows = cur.fetchall()
 
