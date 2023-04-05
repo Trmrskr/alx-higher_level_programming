b07a17c8d1bf335adade5ddfab551636f69a813b
trmrskr <simonalale@gmail.com>
2023-04-05 07:44:17 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,5 +10,11 @@ if __name__ == '__main__':
     from sys import argv
 
     req = request.Request(argv[1])
-    with request.urlopen(req) as res:
-        print(res.headers.get('X-Request-Id'))
+    with request.urlopen(req) as response:
+        """
+        You can simply use the following one liner to print the
+        `X-Request-Id`
+        print(response.headers.get('X-Request-Id'))
+        """
+        header = response.info()
+        print(header['X-Request-Id'])
