d40f75ed89201f71e3006b38db95f944928eb5ea
trmrskr <simonalale@gmail.com>
2023-04-06 06:15:18 -0700

    Time for an interview: Mini edits

--- 0x11-python-network_1/100-github_commits_alt.py
+++ 0x11-python-network_1/100-github_commits_alt.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     commits = r.json()
-    for i in range(10):
-        print(commits[i]['sha'], end=': ')
-        print(commits[i]['commit']['author']['name'])
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
