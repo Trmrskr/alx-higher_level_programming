d8033ed2b13ea233461ffe2fd39ce17d36458703
trmrskr <simonalale@gmail.com>
2023-04-05 20:33:42 -0700

    Response header value #1: Minor edits

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,8 +6,12 @@ the value of the variable X-Request_Id in the response header
 """
 
 if __name__ == '__main__':
+    """
+    An alternative solution is using the following line:
+    `print(res.headers.get('X-Request-Id')`
+    """
     import requests
     from sys import argv
 
     res = requests.get(argv[1])
-    print(res.headers.get('X-Request-Id'))
+    print(res.headers['X-Request-Id'])
