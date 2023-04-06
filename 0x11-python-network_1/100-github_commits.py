1cf3daffc0909090e0f2b38c5436e51987f0ce10
trmrskr <simonalale@gmail.com>
2023-04-06 06:43:21 -0700

    Time for Interview: More correction

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                      .format(argv[2], argv[1]))
     commits = r.json()
-    for i in range(10):
+    length = len(commits) if len(commits) < 10 else 10
+    for i in range(length):
         print(commits[i]['sha'], end=': ')
         print(commits[i]['commit']['author']['name'])
