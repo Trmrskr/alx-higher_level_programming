41ecaa29ec3f46e516a1dab40224a2cee22d577a
trmrskr <simonalale@gmail.com>
2023-04-06 05:22:42 -0700

    My Github! Mini correction

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,8 +9,13 @@ if __name__ == '__main__':
     from sys import argv
     url = 'https://api.github.com/user'
     response = requests.get(url, auth=(argv[1], argv[2]))
+    search_key = 'id'
     try:
         res = response.json()
-        print(res['id'])
     except ValueError:
         print("Not a valid JSON")
+    else:
+        if search_key in res:
+            print(res['id'])
+        else:
+            print(None)
